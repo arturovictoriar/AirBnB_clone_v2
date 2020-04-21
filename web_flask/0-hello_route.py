274cd8094eb45b81d3fcc52d0a457dae9ae77f6a
arvicrin <1231@holbertonschool>
2020-04-21 17:57:24 +0000

    Fix pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
     """Say hello to the client"""
     return 'Hello HBNB!'
