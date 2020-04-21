e464cf9ceb4b56d9cae63f665f2b7995348c7215
arvicrin <1231@holbertonschool>
2020-04-21 17:50:14 +0000

    Add only main exe

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,4 +11,5 @@ def hello_HBNB():
     """Say hello to the client"""
     return 'Hello HBNB!'
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
