77bad92f4bac3d7de97b86c62fd697fb2f57d5b3
arvicrin <1231@holbertonschool>
2020-04-21 20:50:30 +0000

    Fix the strict=false

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,31 +4,31 @@
 """
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
     """Say Hello HBNB to the client"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def HBNB():
     """Say HBNB to the client"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_isfun(text):
     """Say C and a given text to the client"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_iscool(text='is cool'):
     """Say Python and a given text to the client"""
     return 'Python {}'.format(text.replace('_', ' '))
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
