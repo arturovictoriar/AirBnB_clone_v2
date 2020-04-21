d151d9a09f5381d62fb45e574fc8c235b2a3ccd8
arvicrin <1231@holbertonschool>
2020-04-21 17:52:29 +0000

    Add another way to strict_slashes = False

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,10 @@
 """
 from flask import Flask
 app = Flask(__name__)
+pp.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_HBNB():
     """Say hello to the client"""
     return 'Hello HBNB!'
